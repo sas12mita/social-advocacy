cb6fce5feeee4680760cf8cede0068784c027aac
sas12mita <sumitapoudel12@gmail.com>
2025-11-02 22:40:51 +0545

    update registration

---
+++ database/migrations/2025_11_02_164721_create_volunteer_applications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('volunteer_applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->text('motivation')->nullable(); // why they want to join
+            $table->boolean('approved')->default(false); // admin approval
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('volunteer_applications');
+    }
+};
